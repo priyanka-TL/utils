fed39a284cf6f76c39f100ba655840dfa72deb0c
Aman Kumar Gupta <amangupta15jan@gmail.com>
2021-12-30 18:53:28 +0530

    FILES CLOUD STORAGE INTIAL COMMIT

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    GcpFileHelper: require('./lib/gcp/upload'),
+    AwsFileHelper: require('./lib/aws/upload'),
+    AzureFileHelper: require('./lib/azure/upload'),
+};
