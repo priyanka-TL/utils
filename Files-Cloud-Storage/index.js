9b5fc71937d4d093443cecf820ad98460a07e434
rakesh kumar <rakeshkumar@rakeshs-MacBook-Air.local>
2022-11-10 10:21:11 +0530

    oci cloud object storage added

--- Files-Cloud-Storage/index.js
+++ Files-Cloud-Storage/index.js
@@ -4,4 +4,5 @@ module.exports = {
     GcpFileHelper: require('./lib/gcp/upload'),
     AwsFileHelper: require('./lib/aws/upload'),
     AzureFileHelper: require('./lib/azure/upload'),
+    OciFileHelper: require('./lib/oci/upload'),
 };
