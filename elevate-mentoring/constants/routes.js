70156f27f4395438a9c46ca89323a48f14d1fadf
joffinjoy <joffinjoy@gmail.com>
2023-08-23 15:49:33 +0530

    Modified Config

--- elevate-mentoring/constants/routes.js
+++ elevate-mentoring/constants/routes.js
@@ -1,6 +1,35 @@
-module.exports = [
-	{ route: '/v1/get-user-photo', config: [{ type: 'GET' }] },
-	{ route: '/v1/get-user-profile', config: [{ type: 'POST' }] },
-	{ route: '/v1/get-sharelink', config: [{ type: 'PUT' }, { type: 'PATCH' }] },
-	{ route: '/v1/get-user-details', config: [{ type: 'DELETE' }] },
-];
+exports.routesConfigs = {
+	services: ['user', 'mentoring'],
+	routes: [
+		{
+			sourceRoute: '/v1/get-sessions',
+			type: 'GET',
+			priority: 'MUST_HAVE',
+			inSequence: false,
+			orchestrated: false,
+			targetRoutes: [
+				{
+					basePackageName: 'mentoring',
+					packageName: 'elevate-mentoring',
+				},
+			],
+		},
+		{
+			sourceRoute: '/v1/user-signup',
+			type: 'POST',
+			priority: 'MUST_HAVE',
+			inSequence: true,
+			orchestrated: true,
+			targetRoutes: [
+				{
+					basePackageName: 'user',
+					packageName: 'elevate-user',
+				},
+				{
+					basePackageName: 'mentoring',
+					packageName: 'elevate-mentoring',
+				},
+			],
+		},
+	],
+}
