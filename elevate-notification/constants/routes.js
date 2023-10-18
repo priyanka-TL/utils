d768521a8d9968f060b5aa5ce02b24774cf8cc91
VISHNU <apple@Apples-MacBook-Pro.local>
2023-10-18 12:03:43 +0530

    route config syntax error resolved

--- elevate-notification/constants/routes.js
+++ elevate-notification/constants/routes.js
@@ -1,10 +1,10 @@
 module.exports = {
 	routes: [
 		{
-			sourceRoute: 'notification/v1/email/send',
+			sourceRoute: '/notification/v1/email/send',
 			type: 'POST',
 			targetRoute: {
-				path: 'notification/v1/email/send',
+				path: '/notification/v1/email/send',
 				type: 'POST',
 			},
 		}
