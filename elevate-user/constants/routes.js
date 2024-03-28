c510219c57491bd0b4fb4fbd54f748bf5bb49c9d
Nevil <nevil@tunerlabs.com>
2024-03-28 16:02:00 +0530

    added user session APIs

--- elevate-user/constants/routes.js
+++ elevate-user/constants/routes.js
@@ -792,7 +792,29 @@ module.exports = {
 				path: '/user/v1/account/changePassword',
 				type: 'POST',
 			},
-		}
+		},
+		{
+			sourceRoute: '/user/v1/account/sessions',
+			type: 'GET',
+			priority: 'MUST_HAVE',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/user/v1/account/sessions',
+				type: 'GET',
+			},
+		},
+		{
+			sourceRoute: '/user/v1/account/validateUserSession',
+			type: 'POST',
+			priority: 'MUST_HAVE',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/user/v1/account/validateUserSession',
+				type: 'POST',
+			},
+		},
 	],
 }
 
