11458dd1deadfc5dd219a34b2f33ac451d76b81f
Priyanka <priyanka@tunerlabs.com>
2024-05-06 16:03:45 +0530

    updated entity pacakge

--- elevate-entity-management/index.js
+++ elevate-entity-management/index.js
@@ -9,7 +9,7 @@ const getDependencies = () => {
 
 const getPackageMeta = () => {
 	return {
-		basePackageName: 'entity-management',
+		basePackageName: 'entity_management',
 		packageName: 'elevate-entity-management',
 	}
 }
