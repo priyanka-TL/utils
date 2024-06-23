b5f8478ccc89e01caa98115813cf850a294cca21
joffinjoy <joffinjoy@gmail.com>
2024-06-23 13:21:19 +0530

    Added KB Packages

---
+++ kb-mentoring/controllers/orchestrationController.js
@@ -0,0 +1,13 @@
+const routesConfig = require('../constants/routes')
+const mentoringController = require('../controllers/mentoring')
+const orchestrationHandler = async (req, res, responses) => {
+	console.log(req.targetPackages, req.inSequence, req.orchestrated, req.sourceRoute, responses)
+	console.log(req.body)
+	const selectedRouteConfig = routesConfig.routes.find((obj) => obj.sourceRoute === req.sourceRoute)
+	return await mentoringController[selectedRouteConfig.targetRoute.functionName](req, res, responses)
+}
+
+const orchestrationController = {
+	orchestrationHandler,
+}
+module.exports = orchestrationController
---
+++ kb-user/controllers/user.js
@@ -0,0 +1,40 @@
+const routeConfigs = require('../constants/routes')
+const requesters = require('../utils/requester')
+const requestParser = require('../utils/requestParser')
+
+const createUser = async (req, res, responses) => {
+	const selectedConfig = routeConfigs.routes.find((obj) => obj.sourceRoute === req.sourceRoute)
+	return await requesters.post(req.baseUrl, selectedConfig.targetRoute.path, req.body,{
+		'device-info': req.headers['device-info'],
+	})
+}
+const updateUser = async (req, res, responses) => {
+	const selectedConfig = routeConfigs.routes.find((obj) => obj.sourceRoute === req.sourceRoute)
+
+	//const filteredRequestBody = requestParser.transformUpdateUserBody(req.body)
+	return await requesters.patch(req.baseUrl, selectedConfig.targetRoute.path, req.body, req.headers)
+}
+
+const entityTypeRead = async (req, res, responses) => {
+	const selectedConfig = routeConfigs.routes.find((obj) => obj.sourceRoute === req.sourceRoute)
+	return await requesters.post(req.baseUrl, selectedConfig.targetRoute.path, req.body, {
+		'X-auth-token': req.headers['x-auth-token'],
+	})
+}
+
+const loginUser = async (req, res, responses) => {
+	const selectedConfig = routeConfigs.routes.find((obj) => obj.sourceRoute === req.sourceRoute)
+	return await requesters.post(req.baseUrl, selectedConfig.targetRoute.path, req.body,{
+		'captcha-token': req.headers['captcha-token'],
+		'device-info': req.headers['device-info'],
+	})
+}
+
+const userController = {
+	createUser,
+	updateUser,
+	entityTypeRead,
+	loginUser,
+}
+
+module.exports = userController
