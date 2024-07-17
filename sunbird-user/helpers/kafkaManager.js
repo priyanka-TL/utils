bed5feb9fa335eaf0be94de8d2b2b27875876147
joffinjoy <joffinjoy@gmail.com>
2024-07-17 10:29:55 +0530

    Modified Kafka Manager: Non-Blocking Message Processor, Added Internal Access Token ENV Request

--- sunbird-user/helpers/kafkaManager.js
+++ sunbird-user/helpers/kafkaManager.js
@@ -1,28 +1,35 @@
-const {post} = require('../utils/requester')
+const { post } = require('../utils/requester')
 
 const kafkaManager = async (kafkaPackage, environmentVariables) => {
-    try{
-        const {ElevateKafka} = kafkaPackage
+	try {
+		const { ElevateKafka } = kafkaPackage
 
-        const kafka = new ElevateKafka(environmentVariables.SUNBIRD_USER_KAFKA_BROKERS, environmentVariables.SUNBIRD_USER_KAFKA_BROKERS, {
-            packageName: 'sunbird-user',
-        })
+		const kafka = new ElevateKafka(
+			environmentVariables.SUNBIRD_USER_KAFKA_BROKERS,
+			environmentVariables.SUNBIRD_USER_KAFKA_BROKERS,
+			{
+				packageName: 'sunbird-user',
+			}
+		)
 
-        const topics = [environmentVariables.SUNBIRD_USER_USER_UPDATE_KAFKA_TOPIC]
-        const consumer = await kafka.createConsumer(environmentVariables.SUNBIRD_USER_KAFKA_GROUP_ID, topics)
+		const topics = [environmentVariables.SUNBIRD_USER_USER_UPDATE_KAFKA_TOPIC]
+		const consumer = await kafka.createConsumer(environmentVariables.SUNBIRD_USER_KAFKA_GROUP_ID, topics)
 		await kafka.runConsumer(consumer, async (topic, message) => {
-			if(topic == 'sunbird-user-update'){
-                const response = await post(environmentVariables.MENTORING_SERVICE_BASE_URL,environmentVariables.MENTORING_SERVICE_USER_UPDATE_ROUTE, JSON.parse(message.value))
-                console.log("RESPONSE: ",response)
-            }
+			if (topic == environmentVariables.SUNBIRD_USER_USER_UPDATE_KAFKA_TOPIC) {
+				post(
+					environmentVariables.MENTORING_SERVICE_BASE_URL,
+					environmentVariables.MENTORING_SERVICE_USER_UPDATE_ROUTE,
+					JSON.parse(message.value),
+					{
+						INTERNAL_ACCESS_TOKEN: environmentVariables.MENTORING_INTERNAL_ACCESS_TOKEN,
+					}
+				)
+			}
 		})
-
-    }catch(error){
-        console.log(error)
-        throw error
-    }
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
 }
-
-
 
 module.exports = kafkaManager
--- sunbird-user/index.js
+++ sunbird-user/index.js
@@ -9,40 +9,44 @@ const getPackageMeta = () => {
 	}
 }
 
-const packageName = 'sunbird-user';
-const environmentVariablePrefix = packageName.toUpperCase().replace('-','_')
+const packageName = 'sunbird-user'
+const environmentVariablePrefix = packageName.toUpperCase().replace('-', '_')
 
 const requiredEnvs = {
-    [`${environmentVariablePrefix}_KAFKA_CLIENT_ID`]: {
-        "message": `[${packageName}] Required Kafka Brokers Hosts`,
-        "optional": false,
-    },
-    [`${environmentVariablePrefix}_KAFKA_BROKERS`]: {
-        "message": `[${packageName}] Required Kafka Brokers Hosts`,
-        "optional": false,
-    },
-    [`${environmentVariablePrefix}_KAFKA_GROUP_ID`]: {
-        "message": `[${packageName}] Required Kafka Group ID`,
-        "optional": false,
-    },
-    [`${environmentVariablePrefix}_USER_UPDATE_KAFKA_TOPIC`]: {
-        "message": `[${packageName}] Required Kafka Topics`,
-        "optional": false,
-    },
-    MENTORING_SERVICE_BASE_URL: {
-        "message": `[${packageName}] Required Base URL for the Mentoring Service`,
-        "optional": false,
-    },
-    MENTORING_SERVICE_USER_UPDATE_ROUTE: {
-        "message": `[${packageName}] Required Route for User Update in the Mentoring Service`,
-        "optional": false,
-    }
-};
+	[`${environmentVariablePrefix}_KAFKA_CLIENT_ID`]: {
+		message: `[${packageName}] Required Kafka Brokers Hosts`,
+		optional: false,
+	},
+	[`${environmentVariablePrefix}_KAFKA_BROKERS`]: {
+		message: `[${packageName}] Required Kafka Brokers Hosts`,
+		optional: false,
+	},
+	[`${environmentVariablePrefix}_KAFKA_GROUP_ID`]: {
+		message: `[${packageName}] Required Kafka Group ID`,
+		optional: false,
+	},
+	[`${environmentVariablePrefix}_USER_UPDATE_KAFKA_TOPIC`]: {
+		message: `[${packageName}] Required Kafka Topics`,
+		optional: false,
+	},
+	MENTORING_SERVICE_BASE_URL: {
+		message: `[${packageName}] Required Base URL for the Mentoring Service`,
+		optional: false,
+	},
+	MENTORING_SERVICE_USER_UPDATE_ROUTE: {
+		message: `[${packageName}] Required Route for User Update in the Mentoring Service`,
+		optional: false,
+	},
+	MENTORING_INTERNAL_ACCESS_TOKEN: {
+		message: `[${packageName}] Required Internal Access Token of the Mentoring Service`,
+		optional: false,
+	},
+}
 
 module.exports = {
 	routes,
 	packageMeta: getPackageMeta(),
 	packageRouter,
-    dependencyManager,
-    requiredEnvs
+	dependencyManager,
+	requiredEnvs,
 }
