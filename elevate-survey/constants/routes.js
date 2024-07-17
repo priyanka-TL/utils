968d39b471e002edfe975fcc4d79fc857213e3b4
praveenKDass <praveendass@shikshalokam.org>
2024-07-18 01:31:49 +0530

    solutions and programs url added

--- elevate-survey/constants/routes.js
+++ elevate-survey/constants/routes.js
@@ -11,6 +11,16 @@ module.exports = {
 			},
 		},
 		{
+			sourceRoute: '/samiksha/v1/admin/createIndex/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/admin/createIndex/:id',
+				type: 'POST'
+			},
+		},
+		{
 			sourceRoute: '/samiksha/v1/polls/create',
 			type: 'POST',
 			inSequence: false,
@@ -67,6 +77,236 @@ module.exports = {
 			orchestrated: false,
 			targetRoute: {
 				path: '/samiksha/v1/files/getDownloadableUrl',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/create',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/create',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/details/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/details/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/verifyLink/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/verifyLink/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/fetchLink/:id',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/fetchLink/:id',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/update/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/update/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/getDetails/:id',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/getDetails/:id',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/list',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/list',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/isTargetedBasedOnUserProfile/:id',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/isTargetedBasedOnUserProfile/:id',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/detailsBasedOnRoleAndLocation/:id',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/detailsBasedOnRoleAndLocation/:id',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/forUserRoleAndLocation',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/forUserRoleAndLocation',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/addEntitiesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/addEntitiesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/addRolesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/addRolesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/removeEntitiesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/removeEntitiesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/solutions/removeRolesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/solutions/removeRolesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/addEntitiesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/addEntitiesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/addRolesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/addRolesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/removeEntitiesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/removeEntitiesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/removeRolesInScope/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/removeRolesInScope/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/join/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/join/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/create',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/create',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/update/:id',
+			type: 'POST',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/update/:id',
+				type: 'POST'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/details/:id',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/details/:id',
+				type: 'GET'
+			},
+		},
+		{
+			sourceRoute: '/samiksha/v1/programs/list',
+			type: 'GET',
+			inSequence: false,
+			orchestrated: false,
+			targetRoute: {
+				path: '/samiksha/v1/programs/list',
 				type: 'GET'
 			},
 		},
